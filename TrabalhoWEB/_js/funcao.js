cb92c85a6a49e540d566e87ea12beca493a7cacd
VitorSiqueira <32824818+VitorSiqueira@users.noreply.github.com>
2017-12-29 22:10:20 -0200

    Escopo
    Escopo inicial.

---
+++ TrabalhoWEB/_js/funcao.js
@@ -0,0 +1,16 @@
+/*
+Aluna: Marina Margarida de Oliveira
+Matrícula: ##.#.####
+
+Aluno: Vitor de Siqueira Cotta
+Matrícula: 14.1.8143
+
+Disciplina: CSI477 - SISTEMAS WEB I - Turma 11
+Professor: FERNANDO BERNARDES DE OLIVEIRA
+Atividade: Trabalho
+Data: ##/##/2018
+*/
+
+$(function(){
+	
+});	
---
+++ TrabalhoWEB/index.php
@@ -0,0 +1,54 @@
+<!--
+Aluna: Marina Margarida de Oliveira
+Matrícula: ##.#.####
+
+Aluno: Vitor de Siqueira Cotta
+Matrícula: 14.1.8143
+
+Disciplina: CSI477 - SISTEMAS WEB I - Turma 11
+Professor: FERNANDO BERNARDES DE OLIVEIRA
+Atividade: Trabalho
+Data: ##/##/2018
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Marina Margarida de Oliveira, Vitor de Siqueira Cotta">
+        <title>Página Inicial</title>
+        <link rel="icon" type="image/png" sizes="96x96" href="_img/icone.png">
+        <link rel="stylesheet" type="text/css" href="_css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="_css/estilo.css">
+        <script type="text/javascript" src="_js/jquery.js"></script>
+        <script type="text/javascript" src="_js/funcao.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <nav class="navbar navbar-inverse">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">NOME DO SITE</a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Página inicial</a></li>
+                        <li><a href="#">Calcular</a></li>
+                        <li><a href="#">Fale Conosco</a></li>
+                        <li><a href="#">Sobre</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1>Página Inicial</h1>
+                <p>Conteúdo Qualquer</p>
+                <br>
+            </main>
+            <footer>
+                <b>Universidade Federal de Ouro Preto</b> <br>
+                Aluna: Marina Margarida de Olivera <br>
+                E-mail: #@gmail.com <br>
+                Aluno: Vitor de Siqueira Cotta <br>
+                E-mail: Vithorkross@outlook.com <br>
+            </footer>
+        </div>
+    </body>
+</html>
