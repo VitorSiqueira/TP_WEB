4ff4c4243b2de51a677d3f96d1c1767aee614e80
VitorSiqueira <32824818+VitorSiqueira@users.noreply.github.com>
2018-01-05 01:49:34 -0200

    Commit grande, adicionando as fonts e os glyphicons, construindo o formulário do seguro do carro.
    OBS.: adicionar o import do bootstrap.JS em outras páginas, e jquery antes do bootstrap.

--- TrabalhoWEB/simulacaoCarro.php
+++ TrabalhoWEB/simulacaoCarro.php
@@ -20,6 +20,7 @@ Data: ##/##/2018
         <link rel="stylesheet" type="text/css" href="_css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="_css/estilo.css">
         <script type="text/javascript" src="_js/jquery.js"></script>
+        <script type="text/javascript" src="_js/bootstrap.js"></script>
         <script type="text/javascript" src="_js/funcao.js"></script>
     </head>
     <body>
@@ -40,10 +41,178 @@ Data: ##/##/2018
             </header>
             <main>
                 <div class="container-fluid">
-                    <h1>Simulação Carro</h1>
-                    <p>Conteúdo Qualquer</p>
                     <br>
+                    <h1>Fórmulário do Automóvel</h1>
+                    <div class="well">
+                        <p>Este formulário irá coletar os dados e serão mantidos em sigilo. O formulário será encaminha e analisado pela equipe.</p>
+                    </div>
+                    <ul class="nav nav-tabs">
+                        
+                        <li class="active"><a data-toggle="tab" href="#iCliente"><span class="glyphicon glyphicon-user"></span> Dados do Cliente</a></li>
+                        <li><a data-toggle="tab" href="#iCarro">Dados do Carro</a></li>
+                        <li><a data-toggle="tab" href="#iTipoSeguro"><span class=""></span> Tipo de Seguro</a></li>
+                    </ul>
+                    <form action="#">
+                        <div class="tab-content">
+                            <div id="iCliente" class="tab-pane fade in active">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                        <div class="form-group">
+                                            <label for="iNome">Qual é seu nome?</label>
+                                            <input id="iNome" name="nNome" type="text" class="form-control" placeholder="Seu nome completo.">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iCPF">Seu CPF</label>
+                                            <input id="iCPF" name="nCPF" type="text" class="form-control">
+                                        </div>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">Gênero</div>
+                                            <div class="panel-body">
+                                                <label class="radio-inline"><input id="iMasculino" name="nGenero" type="radio">Masculino</label>
+                                                <label class="radio-inline"><input id="iFeminino" name="nGenero" type="radio">Feminino</label>
+                                                <label class="radio-inline"><input id="iNaoBinario" name="nGenero" type="radio">Não Binário</label>
+                                            </div>
+                                        </div>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">Estado Civil</div>
+                                            <div class="panel-body">
+                                                <label class="radio-inline"><input id="iCasado" name="nEstadoCivil" type="radio">Casado(a)</label>
+                                                <label class="radio-inline"><input id="iViuvo" name="nEstadoCivil" type="radio">Viuvo(a)</label>
+                                                <label class="radio-inline"><input id="iDivorciado" name="nEstadoCivil" type="radio">Divorciado(a)</label>
+                                                <label class="radio-inline"><input id="iSolteiro" name="nEstadoCivil" type="radio">Soteiro(a)</label>
+                                                <label class="radio-inline"><input id="iSeparado" name="nEstadoCivil" type="radio">Separado(a)</label>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iFilhos">Filhos</label>
+                                            <select class="form-control" id="iFilhos">
+                                                <option>0</option>
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                                <option>6</option>
+                                                <option>7</option>
+                                                <option>8</option>
+                                                <option>9</option>
+                                                <option>10</option>
+                                            </select>
+                                        </div>
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">Depedências</div>
+                                            <div class="panel-body">
+                                                <div class="checkbox">
+                                                    <label class="checkbox-inline"><input type="checkbox" value="">Bebidas Alcoólicas</label>
+                                                    <label class="checkbox-inline"><input type="checkbox" value="">Tabaco</label>
+                                                    <label class="checkbox-inline"><input type="checkbox" value="">Jogos de Azar</label>
+                                                    <label class="checkbox-inline"><input type="checkbox" value="">Entorpecentes</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iNascimento">Data de nascimento</label>
+                                            <input id="iNascimento" name="nNascimento" type="date" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iExpedicaoHabilitacao">Data de expedição da habilitação</label>
+                                            <input id="iExpedicaoHabilitacao" name="nExpedicaoHabilitacao" type="date" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="iCarro" class="tab-pane fade">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                        <div class="form-group">
+                                            <label for="iFabricante">Fabricante</label>
+                                            <select class="form-control" id="iFabricante">
+                                                <option></option>
+                                                <option>Chevrolet</option>
+                                                <option>Fiat</option>
+                                                <option>Ford</option>
+                                                <option>Nissan</option>
+                                                <option>Hyundai</option>
+                                                <option>Honda</option>
+                                                <option>Peugeot</option>
+                                                <option>Citroen</option>
+                                                <option>Toyota</option>
+                                                <option>Volkswagen</option>
+                                                <option>Suzuki</option>
+                                                <option>Kia</option>
+                                                <option>Outros</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iClasse">Classe</label>
+                                            <select class="form-control" id="iClasse">
+                                                <option></option>
+                                                <option>Micro</option>
+                                                <option>Subcompacto</option>
+                                                <option>Compacto</option>
+                                                <option>Tamanho médio</option>
+                                                <option>Tamanho grande</option>
+                                                <option>Luxo</option>
+                                                <option>Desportivo</option>
+                                                <option>Cabrio</option>
+                                                <option>Roadster</option>
+                                                <option>Minivan</option>
+                                                <option>Mini SUV</option>
+                                                <option>SUV compacto</option>
+                                                <option>SUV de tamanho médio económico</option>
+                                                <option>SUV de tamanho médio</option>
+                                                <option>SUV de tamanho grande</option>
+                                                <option>Pick-up Pequena</option>
+                                                <option>Pick-up de grande porte</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iCarroceria">Carroceria</label>
+                                            <select class="form-control" id="iCarroceria">
+                                                <option></option>
+                                                <option>Hatchbacks</option>
+                                                <option>Sedans</option>
+                                                <option>Station Wagons</option>
+                                                <option>Pick-Ups</option>
+                                                <option>Furgões</option>
+                                                <option>Conversíveis</option>
+                                                <option>Cabriolets</option>
+                                                <option>Targas</option>
+                                                <option>Roadsters</option>
+                                                <option>Coupés</option>
+                                                <option>Vans</option>
+                                                <option>Veículos fora de estrada</option>
+                                                <option>Utilitários esportivos</option>
+                                                <option>Minivans</option>
+                                                <option>Crossovers</option>
+                                                <option>Shooting Break</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="iNumeroChassi">Número do Chassi</label>
+                                            <input id="iNumeroChassi" name="nNumeroChassi" type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="iTipoSeguro" class="tab-content fade">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                        <label class="radio-inline"><input id="iTotal" name="nTipoSeguro" type="radio">Total</label>
+                                        <label class="radio-inline"><input id="iAcidente" name="nTipoSeguro" type="radio">Acidente</label>
+                                        <label class="radio-inline"><input id="iFurto" name="nTipoSeguro" type="radio">Furto</label>
+                                        <div class="checkbox">
+                                            <label class="checkbox-inline"><input type="checkbox" value="">Concordo com os termos</label>
+                                        </div>
+                                        <button type="reset" class="btn btn-default"><span class="glyphicon glyphicon-remove"></span> Apagar</button>
+                                        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-ok"></span> Enviar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
+                <br>
             </main>
             <footer>
                 <b>Universidade Federal de Ouro Preto</b> <br>
